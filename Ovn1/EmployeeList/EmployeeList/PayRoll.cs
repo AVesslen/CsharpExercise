1ce5d8d1604b91a1ccd23960a69dd8b13beaf394
Anna Vesslén <anna_ananaz@hotmail.com>
2022-11-04 10:43:28 +0100

    Skapat nytt projekt Ovn2

---
+++ Ovn1/EmployeeList/EmployeeList/PayRoll.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeList
+{
+    internal class PayRoll
+    {
+        private List<Employee> payRoll;
+
+        public PayRoll()
+        {
+            payRoll = new List<Employee>();
+        }
+
+        public void AddEmployee(string name, int salary)
+        {
+            Employee employee = new Employee(name, salary);
+            payRoll.Add(employee);
+        }
+
+        public List<Employee> GetEmplyees()
+        {
+            //ToDo: Fix. Not good.
+            return payRoll;
+        }
+
+    }
+}
