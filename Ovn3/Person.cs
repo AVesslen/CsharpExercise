29c0519b252faff82467be42bf8c8eec0cf9cc00
Anna Vesslén <anna_ananaz@hotmail.com>
2022-11-08 17:11:00 +0100

    3.1

---
+++ Ovn3/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovn3
+{
+    internal class Person
+    {
+        private int age;
+        private string fName;
+        private string lName;
+        private int height;
+        private int weight;
+          
+        public int Height { get; set; }
+        public int Weight { get; set; }
+                       
+
+        public string FName
+        {
+            get 
+            { 
+                return fName; 
+            }
+            set 
+            {
+                if (value.Length >= 2 && value.Length <= 10)
+                    fName = value;
+                else
+                    throw new ArgumentException("Length of first name needs to be in a valid range");
+            }
+        }
+
+        public string LName
+        {
+            get
+            {
+                return lName;
+            }
+            set
+            {
+                if (value.Length <= 3 && value.Length <= 15)
+                    lName = value;
+                else
+                    throw new ArgumentException("Length of last name needs to be in a valid range");
+            }
+        }
+
+        public int Age
+        {
+            get 
+            { 
+                return age; 
+            }
+            set 
+            {
+                if (value > 0)
+                    age = value;
+                else
+                    throw new ArgumentException ("Age needs to be >0");                
+            }
+        }
+
+       
+
+
+
+    }
+}
---
+++ Ovn3/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Ovn3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            try
+            {
+                person.Age = -2;               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+            person.FName = "A";
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+            person.LName = "Vessssssssleeeeeeeeeen";
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+
+            Console.WriteLine($"{person.FName}{person.LName}{person.Age}");
+        }
+    }
+}
